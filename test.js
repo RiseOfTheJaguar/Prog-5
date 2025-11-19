23a237ebf51af5af18e3bad4b979f3681f8bac85
Jayavardhan.A <133948595+RiseOfTheJaguar@users.noreply.github.com>
2025-11-19 20:40:55 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,16 @@
-const express = require(&#39;express&#39;);
+const express = require('express');
 const app = express();
-app.get(&#39;/&#39;, (req, res) =&gt; res.send(&#39;{ &quot;response&quot;: &quot;Hello From DSCE&quot; }&#39;));
-app.get(&#39;/will&#39;, (req, res) =&gt; res.send(&#39;{ &quot;response&quot;: &quot;Hello World&quot; }&#39;));
-app.get(&#39;/ready&#39;, (req, res) =&gt; res.send(&#39;{ &quot;response&quot;: &quot;Great!, It works!&quot; }&#39;));
+
+app.get('/', (req, res) => {
+  res.send('{ "response": "Hello From DSCE" }');
+});
+
+app.get('/will', (req, res) => {
+  res.send('{ "response": "Hello World" }');
+});
+
+app.get('/ready', (req, res) => {
+  res.send('{ "response": "Great!, It works!" }');
+});
+
 module.exports = app;
